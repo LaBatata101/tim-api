bfdff213d9af89d9599d9e61590f228713fb2b30
LaBatata101 <labatata101@linuxmail.org>
2022-07-23 18:29:08 -0300

    organize models and schemas code into separate folders

--- tim/models/item.py
+++ tim/models/item.py
@@ -1,25 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL
 from sqlalchemy.orm import relationship
 
-from . import Base
-
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), index=True, nullable=False)
-    email = Column(String(100), unique=True, index=True, nullable=False)
-    hashed_password = Column(String(60), nullable=False)
-    is_admin = Column(Boolean, default=False)
-
-    items = relationship("Item", back_populates="owner")
-
-    def __repr__(self) -> str:
-        return (
-            f"User(id={self.id}, name={self.name}, email={self.email}, is_admin={self.is_admin},"
-            f" hashed_password={self.hashed_password}, items={self.items})"
-        )
+from tim.db import Base
 
 
 class Item(Base):
@@ -30,6 +12,7 @@ class Item(Base):
     description = Column(String(240), index=True, default=None)
     bar_code = Column(String(13), index=True)
     price = Column(DECIMAL(10, 2), index=True)
+    # TODO: Instead of storing the path, store the actual image bytes with LargeBinary
     image_path = Column(String, default=None)
     quantity = Column(Integer, default=0)
     owner_id = Column(Integer, ForeignKey("users.id"))
