3d185d9453ee1934b9a2a19698a0698ea421afe5
LaBatata101 <labatata101@linuxmail.org>
2022-07-23 18:27:35 -0300

    require the user to be admin

--- tim/api/routes/users.py
+++ tim/api/routes/users.py
@@ -1,15 +1,20 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from fastapi import APIRouter
 
-from tim.db import schemas, crud
-from tim.api.dependencies import get_current_admin_user, get_db, get_current_user
+from tim import schemas
+from tim.api.dependencies import (get_current_admin_user, get_current_user,
+                                  get_db)
+from tim.db import crud
 
 route = APIRouter(prefix="/users", tags=["users"])
 
 
 @route.post("/register", response_model=schemas.User, status_code=status.HTTP_201_CREATED)
-async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+async def create_user(
+    user: schemas.UserCreate,
+    db: Session = Depends(get_db),
+    _: schemas.User = Depends(get_current_admin_user)
+):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered!")
@@ -47,7 +52,12 @@ async def delete_user(user_id: int, db: Session = Depends(get_db), _: schemas.Us
 
 
 @route.get("/", response_model=list[schemas.User])
-async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_users(
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_db),
+    _: schemas.User = Depends(get_current_admin_user)
+):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
@@ -58,7 +68,7 @@ async def read_user_me(current_user: schemas.User = Depends(get_current_user)):
 
 
 @route.get("/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(user_id: int, db: Session = Depends(get_db), _: schemas.User = Depends(get_current_admin_user)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
